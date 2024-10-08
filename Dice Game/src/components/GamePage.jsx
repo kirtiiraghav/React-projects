018a98400ede617f5ffbfaaca44c577ad4ddbe63
kirti <kirti.20cs@saividya.ac.in>
2024-10-08 16:41:18 +0530

    add ScoreContainer and DiceValueSelector to the GamePage

--- Dice Game/src/components/GamePage.jsx
+++ Dice Game/src/components/GamePage.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import styled from 'styled-components'
+import DiceValueSelector from './DiceValueSelector'
+import ScoreContainer from './ScoreContainer'
 
 export default function GamePage() {
-  return (
-    <div>GamePage</div>
-  )
+    return (
+        <Main>
+            <ScoreContainer />
+            <DiceValueSelector />
+        </Main>
+    )
 }
+
+const Main = styled.main`
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    max-width: 1000px;
+    margin-inline:auto;
+    padding:24px ;
+`
