2bf3770bba0e978dbab68e1ea4587ec8312fa3e0
kirti <kirti.20cs@saividya.ac.in>
2024-10-10 19:29:40 +0530

    apply global style using styled-components

--- Foody Zone/app/src/main.jsx
+++ Foody Zone/app/src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+`
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <GlobalStyle />
+        <App />
+    </StrictMode>,
 )
