8ec9504eacfda6d835ae0d46e091f41e3c111f1f
kirti <kirti.20cs@saividya.ac.in>
2024-10-10 17:36:10 +0530

    create and responsive Header component with filter options and search input

--- Foody Zone/app/src/App.jsx
+++ Foody Zone/app/src/App.jsx
@@ -1,7 +1,11 @@
-function App() {
+import Header from "./components/Header"
+
+export default function App() {
     return (
-        <></>
+        <>
+            <Header />
+        </>
     )
 }
 
-export default App
+
---
+++ Foody Zone/app/src/components/Header.jsx
@@ -0,0 +1,57 @@
+import styled from 'styled-components'
+import logo from '../../public/images/logo.svg'
+import SearchInputField from './SearchInputField'
+import Button from './Button'
+
+export default function Header() {
+    const filterOptions = ['All', 'Breakfast', 'Lunch', 'Dinner']
+    return (
+        <TopSection>
+            <div className='top-header'>
+                <div>
+                    <img src={logo} alt="logo" />
+                </div>
+                <SearchInputField />
+            </div>
+            <div className='filter'>
+                {
+                    filterOptions.map((option, i) => <Button key={i} label={option} />)
+                }
+            </div>
+        </TopSection>
+    )
+}
+
+const TopSection = styled.header`
+    background-color: #323334;
+    padding: 12px;
+    
+    .top-header{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        padding-top: 40px;
+    }
+
+    .filter{
+        display: flex;
+        gap: 14px;
+        justify-content: center;
+        margin-top: 54px;
+        margin-bottom: 31px;
+    }
+
+    @media (max-width: 600px) {
+        .top-header{
+            flex-direction: column;
+            gap: 24px;
+            padding-top: 24px;
+        }
+
+        .filter{
+            margin-top: 24px;
+        }
+    }
+
+
+`
