682686d22c5b9e04bada84a7547688e86de51c7f
kirti <kirti.20cs@saividya.ac.in>
2024-11-19 10:59:15 +0530

    feat: add functionality to input and add items to the todo list

--- To Do List/src/App.jsx
+++ To Do List/src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react'
 import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
 
 function App() {
+    const [todos, setTodos] = useState(['a', 'b', 'c'])
+
+    const handleAddTodo = (newTodoItem) => {
+        const newTodo = [...todos, newTodoItem]
+        setTodos(newTodo)
+    }
     return (
         <main>
-            <TodoInput />
+            <TodoInput handleAddTodo={handleAddTodo} />
+            <TodoList todos={todos} />
         </main>
     )
 }
--- To Do List/src/components/TodoInput.jsx
+++ To Do List/src/components/TodoInput.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function TodoInput() {
+export default function TodoInput({ handleAddTodo }) {
+    const [todo, setTodo] = useState('')
     return (
         <header className='input-header'>
-            <input type="text" placeholder='Enter todo...'/> 
-            <button>Add</button>
+            <input value={todo} onChange={(e) => setTodo(e.target.value)} type="text" placeholder='Enter todo...' />
+            <button onClick={() => {
+                handleAddTodo(todo)
+            }}>Add</button>
         </header>
     )
 }
